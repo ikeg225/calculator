49460b4bf1c9c5787626e70bde36bf78c74be648
Ethan Ikegami <ikeg225@berkeley.edu>
2021-08-16 16:44:14 -0700

    Basic Functions and Buttons

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let curr_input = "";
+let nums = {
+    number: "empty",
+    operator: "empty"
+};
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -25,3 +31,68 @@ function operate(operator, num1, num2) {
         return divide(num1, num2)
     }
 }
+
+const input = document.querySelector('input');
+const buttons = document.querySelectorAll('button[class=num]');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        curr_input += button.textContent;
+        input.value = curr_input;
+    });
+});
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    nums['number'] = "empty";
+    nums['operator'] = "empty";
+    curr_input = "";
+    input.value = "0";
+});
+
+const back = document.getElementById('backspace');
+back.addEventListener('click', () => {
+    curr_input = curr_input.substring(0, curr_input.length - 1);
+    input.value = curr_input;
+});
+
+const plusOrMinus = document.getElementById('plusOrMinus');
+plusOrMinus.addEventListener('click', () => {
+    if (curr_input[0] == '-') {
+        curr_input = curr_input.substring(1, curr_input.length);
+    } else {
+        curr_input = '-' + curr_input;
+    }
+    input.value = curr_input;
+});
+
+const dec = document.getElementById('decimal');
+dec.addEventListener('click', () => {
+    if (!input.value.includes('.')) {
+        curr_input += ".";
+        input.value = curr_input;
+    }
+});
+
+const op = document.querySelectorAll('button[class=op]');
+op.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        if (nums['number'] == 'empty') {
+            nums['number'] = input.value;
+            nums['operator'] = operation.id;
+            curr_input = "";
+        } else {
+            nums['number'] = operate(nums['operator'], parseFloat(nums['number']), parseFloat(curr_input));
+            nums['operator'] = operation.id;
+            input.value = nums['number'];
+            curr_input = "";
+        }
+    });
+});
+
+const equal = document.getElementById('equal');
+equal.addEventListener('click', () => {
+    if (nums['operator'] != 'empty' || curr_input != "") {
+        input.value = operate(nums['operator'], parseFloat(nums['number']), parseFloat(curr_input));
+        nums['number'] = "empty";
+    }
+})
